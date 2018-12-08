c10bb4066827093027d9ac50490dda46505f94a0
Alejandro Emmanuel Burgos <43187850+aburgos43@users.noreply.github.com>
2018-12-08 13:12:57 -0500

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,7 @@
+#Test comment
+print("I could have code like this.") #and the comment after it is ignored
+
+#comments can be used to comment out code so tht the line of code won't run
+#printf("Like this")
+
+print("But THIS will run.")
